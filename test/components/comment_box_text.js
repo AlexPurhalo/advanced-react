e8c81f3af2e08727e19d37172149f79c2e6ecf10
alexpurhalo <alexpurhalo@gmail.com>
2016-07-22 00:13:36 +0300

    comments-submititng-specs

--- test/components/comment_box_text.js
+++ test/components/comment_box_text.js
@@ -11,6 +11,7 @@ describe('CommentBox', () => {
 	it('has the correct class', ()=> {
 		expect(component).to.have.class('comment-box');
 	});
+
 	it('has a text area', ()=> {
 		expect(component.find('textarea')).to.exist;
 	});
@@ -18,4 +19,22 @@ describe('CommentBox', () => {
 	it('has a button', () => {
 		expect(component.find('button')).to.exist;
 	});
+
+	describe('entering some text', () => {
+		beforeEach(() => {
+			component.find('textarea').simulate('change', 'new component')
+		});
+		it('shows that text in the text area', () => {
+			expect(component.find('textarea')).to.have.value('new component')
+		});
+
+		it('when submitted, clears the input', () => {
+			// console.log(component);
+			component.simulate('submit');
+			expect(component.find('textarea')).to.have.value('');
+		});
+	});
+
+
+
 });
